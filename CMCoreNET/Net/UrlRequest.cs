876b18ca559abcdd8f62db65b59599feee3c01d0
Michael Crosby <crosby.michael@gmail.com>
2012-02-19 13:55:04 -0500

    UrlRequest sync obj

--- CMCoreNET/Net/UrlRequest.cs
+++ CMCoreNET/Net/UrlRequest.cs
@@ -4,9 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Net;
 using System.IO;
-using System.Threading;
-using System.Threading.Tasks;
-
 namespace CMCoreNET.Net
 {
     public enum UrlRequestMethod
@@ -27,16 +24,10 @@ namespace CMCoreNET.Net
         #region Fields
 
         private UrlRequestMethod? method;
-        private const int bufferReadSize = 1024;
         private byte[] data;
         private int timeout = 0;
         private WebRequest request;
         private HttpWebResponse response;
-        private UrlRequestEventArgs requestEventObject;
-        private System.Timers.Timer requestTimer;
-        private int currentResponseTime = 0;
-        private SynchronizationContext context;
-        private Action<UrlRequestEventArgs> callback;
 
         #endregion
 
@@ -58,90 +49,47 @@ namespace CMCoreNET.Net
         public WebHeaderCollection Headers { get; set; }
         public CookieCollection Cookies { get; set; }
 
-        private bool HasData {
-            get { return (this.data != null && this.data.Length > 0); }
-        }
-
         #endregion
 
         #region Constructors
 
         public UrlRequest() {
-            this.context = SynchronizationContext.Current;
+            
+        }
+
+        public UrlRequest(string url) {
+            this.Url = url;
         }
 
         #endregion
 
         #region Public Methods
 
-        #region Static Methods
-
-        public static string ProcessStreamToString(Stream stream) {
-            if (stream == null)
-                throw new ArgumentNullException("The requested stream is null");
-
-            using (var reader = new StreamReader(stream)) {
-                string content = reader.ReadToEnd();
-                return content;
-            }
+        public HttpWebResponse Load(string Url) {
+            return Load(Url, UrlRequestMethod.GET, null);
         }
 
-        #endregion
+        public HttpWebResponse Load(
+            string Url, 
+            UrlRequestMethod? method,
+            byte[] data) {
 
-        public void AddHeader(HttpRequestHeader name, string value) {
-            this.Headers.Add(name, value);
-        }
+            this.Url = Url;
+            this.RequestMethod = method;
+            this.data = data;
 
-        public void AddCookie(Cookie cookie) {
-            this.Cookies.Add(cookie);
-        }
+            SetupRequest();
 
-        public UrlRequestEventArgs LoadSync(string url) {
-            this.Url = url;
-            InitRequest();
-            return this.requestEventObject;
-        }
-
-        public void LoadAsync(string url, Action<UrlRequestEventArgs> callback) {
-            this.Url = url;
-            this.callback = callback;
-            Task t = new Task(InitRequest);
-            t.Start();
-        }
-
-        public void Kill() {
-            if (this.request != null) request.Abort();
+            if (data != null)
+                SendRequestData();
+            GetResponse();
+            return this.response;
         }
 
         #endregion
 
 
         #region Private Methods
-
-        private byte[] EncodeString(string data) {
-            if (!string.IsNullOrEmpty(data))
-                return System.Text.Encoding.UTF8.GetBytes(data);
-            return null;
-        }
-
-        private void StartTimer() {
-            this.requestTimer = new System.Timers.Timer();
-            this.requestTimer.Elapsed += 
-                new System.Timers.ElapsedEventHandler(requestTimer_Elapsed);
-            this.requestTimer.Start();
-        }
-
-        private void StopTimerAndDispose() {
-            this.requestTimer.Stop();
-            this.requestTimer.Dispose();
-            this.requestTimer = null;
-        }
-
-        private void requestTimer_Elapsed(object sender, 
-            System.Timers.ElapsedEventArgs e)
-        {
-            this.currentResponseTime++;
-        }
 
         private void CreateRequest() {
             request = WebRequest.Create(this.Url);
@@ -176,52 +124,15 @@ namespace CMCoreNET.Net
             SetupTimeout();
         }
 
-        private void InitRequest() {
-            SetupRequest();
-            StartTimer();
-
-            if (this.HasData)
-                SendRequestData();
-            else
-                GetResponse();
-        }
-
         private void SendRequestData() {
+            request.ContentLength = this.data.Length;
             Stream streamToWrite = request.GetRequestStream();
             streamToWrite.Write(this.data, 0, this.data.Length);
             streamToWrite.Close();
-            GetResponse();
         }
 
         private void GetResponse() {
             response = this.request.GetResponse() as HttpWebResponse;
-
-            StopTimerAndDispose();
-            PopulateEventObject();
-
-            if (this.context != null) {
-                this.context.Post((o) => {
-                    this.callback.Invoke(o as UrlRequestEventArgs);
-                }, this.requestEventObject);
-            }
-        }
-
-        private void PopulateEventObject() {
-            this.requestEventObject = new UrlRequestEventArgs();
-            this.requestEventObject.ResponseTime = this.currentResponseTime;
-            this.requestEventObject.ContentLength = response.ContentLength;
-            this.requestEventObject.ContentType = response.ContentType;
-            this.requestEventObject.StatusCode = response.StatusCode;
-            this.requestEventObject.Headers = response.Headers;
-            this.requestEventObject.Cookies = response.Cookies;
-            this.requestEventObject.ResponseStream = response.GetResponseStream();
-        }
-
-        private void CleanupSelf() {
-            this.requestEventObject = null;
-            this.response.Close();
-            this.response = null;
-            this.request = null;
         }
 
         #endregion
---
+++ CMCoreNET/Web/WebServer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CMCoreNET.Web
+{
+    public class WebServer
+    {
+        #region Fields
+
+        private string Url;
+
+        #endregion
+
+        #region Properties
+
+        public bool IsAlive { get; }
+
+        #endregion
+
+        public WebServer() { }
+
+        public WebServer(string url) {
+            this.Url = url;
+        }
+
+        #region Public Methods
+
+        public void Post() { 
+            
+        }
+
+        public void Head() { 
+        
+        }
+
+        public void Get() { 
+        
+        }
+
+        #endregion
+
+        #region Private Methods
+
+
+
+        #endregion
+    }
+}
