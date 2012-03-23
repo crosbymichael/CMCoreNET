f873dbd9597ac7c907c6e5f588e6ed6e2af9ec6b
Michael Crosby <crosby.michael@gmail.com>
2012-03-23 17:28:22 -0400

    Linq extensions

---
+++ CMCoreNET/Extensions/LinqExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CMCoreNET
+{
+    public static class LinqExtensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> helper, Action<T> action)
+        {
+            foreach (T item in helper)
+            {
+                action(item);
+            }
+        }
+
+        public static bool IsEmpty<T>(this IEnumerable<T> helper)
+        {
+            return helper.Count() == 0;
+        }
+    }
+}
